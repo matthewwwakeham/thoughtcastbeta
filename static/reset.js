c30ff93b39394b2918c077363c0bda3deb0624cd
Matthew Wakeham <98248489+matthewwwakeham@users.noreply.github.com>
2024-04-30 10:58:27 -0500

    Update reset.js

--- static/reset.js
+++ static/reset.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 
 import {
-    getAuth,
+    getAuth, sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 
 const firebaseConfig = {
@@ -11,23 +11,25 @@ const firebaseConfig = {
     storageBucket: "thoughtcast-5ca30.appspot.com",
     messagingSenderId: "41273661048",
     appId: "1:41273661048:web:df2b8420c11546762bff8d",
-  };
+};
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const resetForm = document.getElementById('resetForm');
+const userEmail = document.querySelector("#userEmail");
+const resetButton = document.querySelector("#resetButton");
 
-resetForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
+const resetPassword = async () => {
+  const resetEmail = userEmail.value;
+  try {
+    await sendPasswordResetEmail(auth, resetEmail);
+    alert("Please check your email for instructions on resetting your password.");
+    window.location.href = "index.html";
+  } catch (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode + " " + errorMessage);
+  }
+};
 
-    firebase.auth().sendPasswordResetEmail(email)
-        .then(() => {
-            alert('Password reset email sent. Check your inbox.');
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage);
-        });
-});
+resetButton.addEventListener("click", resetPassword);
